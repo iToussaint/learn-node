1120c1bb878eac6a3dbf506bbcee068d6564a22a
iToussaint <iradukundatoussaint01@gmail.com>
2026-05-05 07:03:45 +0200

    Streams and buffers

--- streams.js
+++ streams.js
@@ -1,7 +1,19 @@
 import fs from "fs";
 
-const readStream = fs.createReadStream("./docs/file.txt", {encoding: 'utf-8'});
+const readStream = fs.createReadStream("./docs/file.txt", {
+  encoding: "utf-8",
+});
+const writeStream = fs.createWriteStream("./docs/file1.txt");
 
 readStream.on("data", (chunk) => {
   console.log(chunk);
+  writeStream.write(chunk)
 });
+
+
+
+// piping
+
+readStream.pipe(writeStream)
+
+// there is also duplex stream
