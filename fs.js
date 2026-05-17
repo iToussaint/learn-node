25abc6c78dbc24bbdf7974fdc3421642589756b8
iToussaint <iradukundatoussaint01@gmail.com>
2026-05-17 11:10:49 +0200

    Add writeAsync.txt and writeSync.txt for file writing examples

--- fs.js
+++ fs.js
@@ -12,3 +12,18 @@ fs.readFile("./fs.txt", "utf-8", (error, data) => {
 const contentSync = fs.readFileSync("./fs.txt", "utf-8");
 
 console.log(contentSync);
+
+fs.writeFileSync("./writeSync.txt", " and append instead of overriding", {
+  flag: "a",
+});
+
+fs.writeFile(
+  "./docs/writeAsync.txt",
+  " that is inside a folder",
+  { flag: "a" },
+  (error) => {
+    if (error) {
+      console.log(error);
+    }
+  },
+);
