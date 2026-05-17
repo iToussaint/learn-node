b51ce51f3339438c02124813dba378e6fbdba5cd
iToussaint <iradukundatoussaint01@gmail.com>
2026-05-17 11:52:59 +0200

    Implement file streaming with read and write operations in streams.js

--- streams.js
+++ streams.js
@@ -1,5 +1,3 @@
-// import fs from "fs";
-
 // const readStream = fs.createReadStream("./docs/file.txt", {
 //   encoding: "utf-8",
 // });
@@ -19,3 +17,22 @@
 const buffer = new Buffer("Toussaint");
 
 console.log(buffer.toJSON());
+
+// console.log(buffer.from());
+
+const fs = require("fs");
+
+// add the object with encoding property set to utf-8, otherwise you will be receiving the buffer in hexadecimal instead of the real content and in that very object you can specify the chunk size which is by default 64kB in bytes
+const readableStream = fs.createReadStream("./docs/file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 1024,
+});
+
+const writeableStream = fs.createWriteStream("./fs.txt", { flags: "a" });
+
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+  console.log("==================");
+
+  writeableStream.write(chunk);
+});
